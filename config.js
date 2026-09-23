adfd5277b9c5a1acb6eecf9ef672c31ed9510853
DogTj <dzuraevdzurabek25@gmail.com>
2026-09-23 15:32:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyDSAV6QBpnF09SCNY4NhS3Cf2p7iY0HLA",
+  apiKey: "AIzaSyDSAV6QBpnfPO9SCNY4NhS3Cf2p7iYOHlA",
   authDomain: "shokh-it.firebaseapp.com",
   projectId: "shokh-it",
   storageBucket: "shokh-it.firebasestorage.app",
   messagingSenderId: "1043196177906",
-  appId: "1:1043196177906:web:c5bbe0bb1878571871cf6b"
+  appId: "1:1043196177906:web:c5bbe0bb1878571871cf6b",
 };
